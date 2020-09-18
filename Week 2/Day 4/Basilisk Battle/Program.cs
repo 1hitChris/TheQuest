e34677cb7146d6b610b51dbbdd1fe0fbdb89b4ef
1hitChris <71494159+1hitChris@users.noreply.github.com>
2020-09-18 12:00:00 +0200

    Week 2 missions

---
+++ Week 2/Day 4/Basilisk Battle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Arthas", "Jaina", "Uther", "Anduin" };
+            
+            {
+                Console.WriteLine($"A party of warriors ({names[0]}, {names[1]}, {names[2]} and {names[3]}) descends into the dungeon.");
+            }
+            int randomNumber;
+            int bosshp = 16;
+            Random rndm = new Random();
+            randomNumber = rndm.Next(1, 7);
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                int d8 = rndm.Next(1, 9);
+                bosshp = bosshp + d8;
+               
+            }
+            
+            Console.WriteLine($"A basilisk with {bosshp} HP appears!");
+
+
+            int damage;
+            Random dmg = new Random();
+            damage = dmg.Next(1, 7);
+            int sworddmg = damage + damage;
+
+            while (bosshp > 0)
+            {
+                Console.Write($"{names[0]} deals {sworddmg} dmg.");
+                Console.WriteLine($" Basilisk has {bosshp - sworddmg} HP left.");
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Week 2/Day 4/Generate Character stats/Character stats/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace day4d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfCalculatedCharacteristics = new List<int> { };
+            var random = new Random();
+            var charactersisticSorter = new List<int> { };
+            var charactersisticSorter2 = new List<int> { };
+            var charactersisticSorter3 = new List<int> { };
+            int sumOfCharacteristics = 0;
+            int sumOfCharacteristics2 = 0;
+            int sumOfCharacteristics3 = 0;
+
+
+
+
+            Console.Write("You roll ");
+            for (int j = 0; j < 4; j++)
+            {
+                int characteristic = random.Next(1, 7);
+                charactersisticSorter.Add(characteristic);
+                Console.Write(characteristic);
+                if (j < 3)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(". ");
+                }
+            }
+
+            charactersisticSorter.Sort();
+            charactersisticSorter.RemoveAt(0);
+
+
+
+
+            foreach (var chara in charactersisticSorter)
+            {
+                sumOfCharacteristics += chara;
+            }
+
+
+
+            Console.WriteLine("The ability score is " + sumOfCharacteristics + ".");
+            listOfCalculatedCharacteristics.Add(sumOfCharacteristics);
+
+            Console.Write("You roll ");
+            for (int j = 0; j < 4; j++)
+            {
+                int characteristic = random.Next(1, 7);
+                charactersisticSorter2.Add(characteristic);
+                Console.Write(characteristic);
+                if (j < 3)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(". ");
+                }
+            }
+
+            charactersisticSorter2.Sort();
+            charactersisticSorter2.RemoveAt(0);
+
+
+
+
+            foreach (var chara in charactersisticSorter2)
+            {
+                sumOfCharacteristics2 += chara;
+            }
+
+
+
+            Console.WriteLine("The ability score is " + sumOfCharacteristics2 + ".");
+            listOfCalculatedCharacteristics.Add(sumOfCharacteristics2);
+
+            Console.Write("You roll ");
+            for (int j = 0; j < 4; j++)
+            {
+                int characteristic = random.Next(1, 7);
+                charactersisticSorter3.Add(characteristic);
+                Console.Write(characteristic);
+                if (j < 3)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(". ");
+                }
+            }
+
+            charactersisticSorter3.Sort();
+            charactersisticSorter3.RemoveAt(0);
+
+
+
+
+            foreach (var chara in charactersisticSorter3)
+            {
+                sumOfCharacteristics3 += chara;
+            }
+
+
+
+            Console.WriteLine("The ability score is " + sumOfCharacteristics3 + ".");
+            listOfCalculatedCharacteristics.Add(sumOfCharacteristics3);
+
+
+
+
+            listOfCalculatedCharacteristics.Sort();
+            Console.Write("Your available ability scores are ");
+            int test = 0;
+            foreach (var chara in listOfCalculatedCharacteristics)
+            {
+                Console.Write(chara);
+                // int test = 0;
+                if (test < 2)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+                test++;
+            }
+
+        }
+    }
+}
