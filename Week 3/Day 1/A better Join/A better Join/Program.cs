da2aadaadf9e6170a1112e9262302dd64d896894
1hitChris <71494159+1hitChris@users.noreply.github.com>
2020-09-22 17:51:29 +0200

    Week 3 - Day 1

---
+++ Week 3/Day 1/A better Join/A better Join/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace A_better_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var heroes = new List<string> { "Arthas", "Jaina", "Uther", "Anduin" };
+            string result = JoinWithAnd(heroes, true);
+            string result2 = JoinWithAnd(heroes, false);
+            Console.WriteLine(result2);
+            Console.WriteLine(result);
+           
+        }
+
+        static string JoinWithAnd(List<string> items, bool useSerialComma = true)
+        {              
+            
+            int count = items.Count;
+
+            if (count == 0)
+            {
+                return "";
+            }
+            else if (count == 1)
+            {
+                return items[0];
+            }
+            else if (count == 2)
+            {
+                return items[0] + " and " + items[1];    
+            }
+
+            else
+            {
+                var itemsCopy = new List<string>(items);
+                if (useSerialComma == true)
+                {
+                    string lastitem = itemsCopy[itemsCopy.Count - 1];
+                    lastitem = "and " + lastitem;
+                    itemsCopy[itemsCopy.Count - 1] = lastitem;
+
+                    // itemsCopy[itemsCopy.Count - 1] = " and " + itemsCopy[itemsCopy.Count - 1];
+                }
+                else
+                {
+                    string last2items = items[items.Count -2] + " and " + items[items.Count - 1];
+                    itemsCopy[items.Count -2] = last2items;
+                    itemsCopy.RemoveAt(itemsCopy.Count -1);
+                                  
+                
+                }
+                
+                return String.Join(", ", itemsCopy);
+                
+            }
+            //return "";
+        }
+
+
+    }
+}
---
+++ Week 3/Day 1/Ordinal numbers/Ordinal numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ordinal_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 220; i++)
+            {
+                Console.WriteLine(Ordinalnumbers(i));
+            }
+
+        }
+        static string Ordinalnumbers(int number)
+        {
+
+            int lastnumber = number % 10;
+
+
+            if (number > 10)
+            {
+
+                int nmb2 = number / 10;
+                int secondlastnmb = nmb2 % 10;
+                if (secondlastnmb == 1)
+                {
+                    return number + "th";
+                }
+            }
+
+            if (lastnumber == 1)
+            {
+                return number + "st";
+            }
+            else if (lastnumber == 2)
+            {
+                return number + "nd";
+            }
+            else if (lastnumber == 3)
+            {
+                return number + "rd";
+            }
+
+            return number + "th";
+
+
+
+        }
+    }
+}
