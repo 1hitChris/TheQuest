e742a49641062a81256ab65db0837ed748e1f098
1hitChris <71494159+1hitChris@users.noreply.github.com>
2020-10-08 13:16:21 +0200

    Week 5 Day 3 ASCII Chart

---
+++ Week 5/ASCII chart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ASCII_chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 255; i++)
+            {
+                var chars = new[]
+               {
+                    (char)(i)
+                };
+                Console.Write($"{i} = ");
+                Console.WriteLine(chars);
+            }
+            
+
+        }
+
+    }
+}
